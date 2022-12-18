1f693f8e34d1dd6e9ab22738a3cf86363f68d58e
Nikitha <nikithamashetti434@gmail.com>
2022-12-18 19:49:56 +0530

    added count of length

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+
+
+let len =document.getElementById("letterCount");
+
+let str =document.getElementById("evaluatedText");
+
+str.addEventListener('keyup' ,(e) =>{
+
+
+    let str1=str.value;
+    
+    document.getElementById("length").innerHTML= str1.length;
+
+})
+
+
+
+
+
